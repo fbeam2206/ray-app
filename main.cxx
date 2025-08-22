a627f59f364138ac408bdd53d5c01f942db21767
fbeam2206 <21293109+fbeam2206@users.noreply.github.com>
2025-08-22 14:55:47 -0500

    Mass Methods

--- main.cxx
+++ main.cxx
@@ -1,4 +1,5 @@
 #include <stdio.h>
+#include "src/class/mass.h"
 #include "src/class/winclass.h"
 #include "src/class/drawcircle.h"
 
@@ -18,16 +19,24 @@
 
 int main(){
 
+  Mass test;
   Window mainWin; 
   mainWin.InitWin(800, 500, (char*)"Window");
   float angle, r;
   r = 100.0;
 
+  test.SetInitPos(Vector3{(float)mainWin.GetWidth()/2, (float)mainWin.GetHeight()/2, 0});
+  test.SetAcc(Vector3 {1, 1, 0});
+
+
   while(!WindowShouldClose()){
     BeginDrawing();
     ClearBackground(BLACK);
     DrawText("Open", 0, 0, 24, RED);
-    DrawPixelV(Vector2Add(MoveInACircle(&angle, r), {400, 250}), RAYWHITE);
+    test.UpdateVel();
+    test.UpdatePos();
+    DrawPixelV(test.GetPos2D(), RAYWHITE);
+    //DrawPixelV(Vector2Add(MoveInACircle(&angle, r), {400, 250}), RAYWHITE);
     EndDrawing();
   }
 
--- src/class/mass.h
+++ src/class/mass.h
@@ -15,7 +15,37 @@ class Mass
   private:
     double _mass;
     Vector3 _pos;
+    Vector3 _vel;
+    Vector3 _acc;
 
   public:
-    
+    void UpdatePos(){
+      this->_pos.x += (0.5)*this->_acc.x*GetFrameTime()*GetFrameTime() + this->_vel.x*GetFrameTime(); 
+      this->_pos.y += (0.5)*this->_acc.y*GetFrameTime()*GetFrameTime() + this->_vel.y*GetFrameTime(); 
+      this->_pos.z += (0.5)*this->_acc.z*GetFrameTime()*GetFrameTime() + this->_vel.z*GetFrameTime(); 
+    }
+
+    void UpdateVel(){
+      this->_vel.x += this->_acc.x*GetFrameTime();
+      this->_vel.y += this->_acc.y*GetFrameTime();
+      this->_vel.z += this->_acc.z*GetFrameTime();
+    }
+
+    void SetAcc(Vector3 acc){
+      this->_acc.x = acc.x;
+      this->_acc.y = acc.y;
+      this->_acc.z = acc.z;
+    }
+
+    void SetInitPos(Vector3 pos){
+      _pos = pos;
+    }
+
+    Vector3 GetPos(){
+      return this->_pos;
+    }
+
+    Vector2 GetPos2D(){
+      return {this->_pos.x, this->_pos.y};
+    }
 };
